07a617b9d31ab4ccc9d44e734a8bd5ea45732ac8
“윤재홍” <a287933@naver.com>
2025-11-01 19:28:38 +0900

    feat():구글폼데이터 화면에 배치하고 css로 이동하는 기능구현 추가로 목업데이터 구현넣어놓음

--- greedy_conference/src/App.jsx
+++ greedy_conference/src/App.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+const 최대높이= 7000;
+const 기본스크롤이동속도= 1;
+
   const [formData, setFormData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -35,6 +38,33 @@ function App() {
 
     fetchData();
   }, []); 
+//여기따지 대충 서버에서 데이터 가져오는 내용
+
+
+  const listRef = useRef(null);
+  const heightRef = useRef(최대높이);
+  //usestate쓰면 계속 재렌더링되서 useRef사용
+  useEffect(() => {
+    let animationFrameId;
+//animationFrameId:requestAnimationFrame함수에 대한 고유 id
+// ->무한루프인 animateScroll을 끝낼 때 이 id로 구별해서 판별
+    const animateScroll = () => {
+      heightRef.current -= 기본스크롤이동속도;
+      if (listRef.current) {
+        listRef.current.style.transform = `translateY(-${heightRef.current}px)`;
+      }
+
+      animationFrameId = requestAnimationFrame(animateScroll);
+      //requestAnimationFrame 화면주사율(60f 에 맞게 알아서 렌더링햊는 함수)
+    };
+
+    animationFrameId = requestAnimationFrame(animateScroll);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      //requestAnimationFrame 중단
+    };
+  }, []);
 
   if (loading) {
     return <div>데이터를 불러오는 중...</div>;
@@ -42,16 +72,19 @@ function App() {
 
   return (
     <div>
-      <h2>구글 폼 응답 데이터</h2>
-      <ul>
+      <h2 style={{position: 'fixed', top: '300px' ,left:'100px'}}>기린기린 그림들어갈자리</h2>
+      <ul ref={listRef} style={{ position: 'relative', height: '100vh' }}>//이거 매직넘버 수정해야함
         {formData.map((item, index) => (
-    
-          <li key={item.timestamp || index}>
-            <strong>{item.timestamp}:</strong> 
-            <br />
-            {item.question1}
-            <br />
-            {item.question2}
+          <li
+            key={item.timestamp || index}
+            style={{
+              position: 'absolute',
+              top: `${10000 - item.question2 * 10}px`,//이거 매직넘버 수정해야함
+              left: `600px`,
+              whiteSpace: 'nowrap',
+            }}
+          >
+            {item.question1} {item.question2}
           </li>
         ))}
       </ul>
@@ -60,4 +93,3 @@ function App() {
 }
 
 export default App;
-
